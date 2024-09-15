2b9cd1bb6e82c4817d327043c4e35d859134a994
SusmithaPerikala12 <susmitha.perikala@in.verizon.com>
2024-09-15 13:33:50 +0530

    Add a computed property ability

--- Creature.playground/Contents.swift
+++ Creature.playground/Contents.swift
@@ -16,6 +16,14 @@ struct Creature{
         self.isGood = isGood
         self.magicPower = magicPower
     }
+    
+    // Computed Property
+    var ability:String
+    {
+        get{
+            return "\(self.name) special ability is \(fibonacciAbility(n: magicPower))"
+        }
+    }
 }
 
 //Creating 3 different Creatures.
@@ -23,3 +31,12 @@ struct Creature{
 var creature1 = Creature(name: "Unicorn", description: "white horse with a spiraling horn on its forehead", isGood: true, magicPower: 5)
 var creature2 = Creature(name: "Dragon", description: "large lizard with scaly skin", isGood: false, magicPower: 10)
 var creature3 = Creature(name: "Phoenix", description: "a mythical bird that symbolizes immortality", isGood: true, magicPower: 15)
+
+// to get nth fibonacci number
+func fibonacciAbility(n: Int) -> Int{
+    if n < 2{
+        return n
+    }
+    return fibonacciAbility(n: n-1) + fibonacciAbility(n: n-2)
+    
+}
